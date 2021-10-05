7d80a6ea5b5108fb1d3836ac1e81e67687f40761
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-04 22:05:20 -0500

    filled in model validations

--- app/model.js
+++ app/model.js
@@ -1,16 +1,41 @@
 const validate = (state)=>{
   const ret = [];
-  if(state.type==="cardio"){} 
-  else if(state.type==="resistance") {} 
-  else {
-  if (!state.type){}
+  if(state.type==="cardio"){
+    if(!state.distance){
+      ret.push("Exercise distance is required");
+    }
+  } else if(state.type==="resistance") {
+    if(!state.sets){
+      ret.push("Exercise sets are required");
+    }
+    if(!state.reps){
+      ret.push("Exercise reps is required");
+    }
+    if(!state.weight){
+      ret.push("Exercise weight is required");
+    }
+  } else {
+    if (!state.type){
+      ret.push("Exercise type is required");
+    }
   }
-  if (!state.name){}
-  if (!state.duration){}
+
+  if (!state.name){
+    ret.push("Exercise name is required")
+  }
+  if (!state.duration){
+    ret.push("Exercise duration is required")
+  }
   return ret;
 };
 
-const validateNum = ()=>({});
+const validateNum = (state)=>({
+  ...state,
+  distance: Number(state.distance),
+  sets: Number(state.sets),
+  reps: Number(state.reps),
+  weight: Number(state.weight),
+});
 
 export default {
   createExercise(){
