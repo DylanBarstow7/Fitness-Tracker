10d4570bf3c8a896d7d2b6ef7253bab750fa4d8a
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-04 21:25:57 -0500

    beginning controller.s

---
+++ app/controller.js
@@ -0,0 +1,22 @@
+import client from "./loader.js";
+
+const workoutController = client.db("workout_db").collection("workouts");
+
+export default {
+    index() {
+      const workouts = workoutController.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" }
+        }
+      },]).toArray()
+      return workouts;
+    },
+    // async update method which includes ID & updated exercise
+    async update(id, newExercise) {
+      // Find the desired workout by finding through the ID
+      const workoutById = await workoutController.findOne({_id:ObjectId(id)});
+      const updatedWorkout = workoutModel.createExercise(workoutById,newExercise);
+      return workoutController.replaceOne({_id:ObjectId(id)},updatedWorkout);
+    },
+};
